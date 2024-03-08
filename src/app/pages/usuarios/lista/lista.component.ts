eecc9e7b1ade04b71f70e43f915d865756a9b67e
Andres Felipe Ramirez Sierra <33188183+AndresR698@users.noreply.github.com>
2024-03-07 21:50:00 -0500

    feat
    1. Se agrega mensaje de alerta para el método de elimina.
    2. Se cambia el diseño del formulario de editar Usuario.

--- src/app/pages/usuarios/lista/lista.component.ts
+++ src/app/pages/usuarios/lista/lista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular'; //1. Importar el AlertController
 import {
   IListUsersRequest,
   IListUsersResponse,
@@ -19,14 +20,40 @@ export class ListaUsuariosComponent implements OnInit {
   constructor(
     public dataService: SessionDataService,
     private usersService: UsersService,
-    private router: Router
+    private router: Router,
+    private alertController: AlertController //2. Definirlo en el constructor
   ) {}
-  public handlerConfirmDelete(userId: number) {
-    this.usersService.deleteUser(userId).subscribe((response: any) => {
-      if (response) {
-        this.loadData();
-      }
+  // 3. Cambiar la a funcion a asyncrona (async)
+  public async handlerConfirmDelete(userId: number) {
+    /**
+     * 4. Definir una constante llamado alert y asignarle la respuesta del metodo create de la clase alertController proveniente del constructor.
+     */
+    const alert = await this.alertController.create({
+      header: '¿Seguro desea eliminar el usuario?', //Le agrega un encabezado al alert
+      buttons: [
+        //Crea 2 botones
+        {
+          text: 'Ok', //Texto del boton
+          handler: () => {
+            //Funcion de confirmacion
+            //Agrega el metodo o la funcion correspondiente, puede ser eliminar o cancelar.
+            this.usersService.deleteUser(userId).subscribe((response: any) => {
+              if (response) {
+                this.loadData();
+              }
+            });
+          },
+        },
+        //Boton de cancelar
+        {
+          text: 'Cancelar', //Texto del boton
+          role: 'cancel', //Role del boton es cancelar.
+        },
+      ],
     });
+
+    //Llamar el alert
+    await alert.present();
   }
   public goToEdit(user: IUser) {
     const state = { user };
@@ -50,7 +77,10 @@ export class ListaUsuariosComponent implements OnInit {
         this.listOfData = response.data;
       });
   }
-  ngOnInit(): void {
+
+  ngOnInit(): void {}
+
+  ionViewWillEnter(): void {
     this.loadData();
     this.isAdmin = this.dataService.getUserData().user_type_id === 1;
   }
